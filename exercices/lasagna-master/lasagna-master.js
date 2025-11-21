d5c12b393263a0284024881fac77c6fcb6eb4938
Elias <144105283+EviV2@users.noreply.github.com>
2025-11-21 15:42:56 +0100

    Feat: (Lasagna-master) Terminer l'exercice
    [120][DONE]

--- exercices/lasagna-master/lasagna-master.js
+++ exercices/lasagna-master/lasagna-master.js
@@ -1,4 +1,3 @@
-/// <reference path="./global.d.ts" />
 // @ts-check
 
 /**
@@ -10,3 +9,95 @@
  *   ...
  * }
  */
+
+/**
+* 
+ * @param {number} time
+ * @returns {string}
+
+ */
+export function cookingStatus(time) {
+
+    if (time == 0) {
+        return "Lasagna is done."
+    }
+    else if (time === null || time === undefined) {
+        return "You forgot to set the timer."
+    }
+    else {
+        return "Not done, please wait."
+    }
+}
+/**
+* 
+ * @param {String[]} layers
+ * @param {Number} timePerLayer
+ * @returns {number}
+ 
+ */
+export function preparationTime(layers, timePerLayer) {
+
+    if (timePerLayer === null || timePerLayer === undefined || timePerLayer < 1) {
+        timePerLayer = 2 //temps si aucun donner
+    }
+
+    return layers.length * timePerLayer
+
+}
+
+/**
+* 
+ * @param {String[]} ingredient
+ * @returns {Object} 
+ */
+export function quantities(ingredient) {
+
+    let sauceLayers = 0;
+    let noodlesLayers = 0;
+    ingredient.forEach(Element => {
+        if (Element == "sauce") {
+            sauceLayers += 0.2
+        }
+        else if (Element == "noodles") {
+            noodlesLayers += 50
+        }
+
+    });
+    return {
+        noodles: noodlesLayers,
+        sauce: sauceLayers
+
+    }
+
+}
+
+/**
+* 
+ * @param {String[]} friendIngredient
+ * @param {String[]} myIngredient
+
+ 
+ */
+export function addSecretIngredient(friendIngredient, myIngredient) {
+
+    let secretIngredient = friendIngredient[friendIngredient.length - 1]
+    myIngredient.push(secretIngredient)
+}
+
+/**
+* 
+ * @param {object} recipe
+ * @param {Number} portions
+ * @returns {Object}
+ 
+ */
+export function scaleRecipe(recipe, portions) {
+    let diviser = portions / 2;
+    const element = { ...recipe }
+    Object.keys(element).forEach(key => {
+        Object(element)[key] = Object(element)[key] * diviser;
+    })
+
+
+    return element
+}
