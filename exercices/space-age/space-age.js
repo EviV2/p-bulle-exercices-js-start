025ff7d05271eda0b317087d83481296e23ec104
Elias <144105283+EviV2@users.noreply.github.com>
2025-11-28 15:50:34 +0100

    Feat (space-age) Terminer l'exercice
    [DONE][60]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,32 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+import { errorMessage } from "../lucky-numbers/lucky-numbers";
+
+export const age = (planete, age) => {
+
+  if (planete === null || planete === undefined) {
+
+
+  }
+  else {
+    let map = new Map();
+    age = age / (60 * 60 * 24 * 365.25)
+    map.set("mercury", 0.2408467)
+    map.set("venus", 0.61519726)
+    map.set("earth", 1.0)
+    map.set("jupiter", 11.862615)
+    map.set("saturn", 29.447498)
+    map.set("uranus", 84.016846)
+    map.set("neptune", 164.79132)
+    map.set("mars", 1.8808158)
+    if (!isNaN(map.get(planete))) {
+      return Math.round(age / map.get(planete) * 100) / 100
+    }
+    else {
+      throw new Error("not a planet")
+
+    }
+  }
+
 };
