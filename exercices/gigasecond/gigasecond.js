0c67e0c6331eaaa4f5459693646db1f5fae68bc4
Elias <144105283+EviV2@users.noreply.github.com>
2025-12-05 14:06:14 +0100

    Feat: (corriger l'exercice gigaseconds)
    [DONE][10]

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
+// 1e12 est de la notation scientifique
+//1 est le coefficient e est la puissance de 10 et  12 est l'exposant (10^12 = 1 000 000 000 000) Source: https://calculator.name/scientific-notation-to-decimal/1e12
 export const gigasecond = (date) => {
   return new Date(date.getTime() + 1e12)
 
