1e1ef27f7607830def7018da2bc2480bc1f97b89
Elias <144105283+EviV2@users.noreply.github.com>
2025-12-12 15:12:45 +0100

    Terminer l'experience (mixed-juices)
    [DONE][30]

--- exercices/mixed-juices/mixed-juices.js
+++ exercices/mixed-juices/mixed-juices.js
@@ -11,7 +11,23 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Remove this line and implement the function');
+  let time = 0;
+  if (name == "Pure Strawberry Joy") {
+    time = 0.5
+  }
+  else if (name == "Energizer" || name == "Green Garden") {
+    time = 1.5
+  }
+  else if (name == "Tropical Island") {
+    time = 3
+  }
+  else if (name == "All or Nothing") {
+    time = 5
+  }
+  else {
+    time = 2.5
+  }
+  return time;
 }
 
 /**
@@ -23,7 +39,27 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Remove this line and implement the function');
+  let i = 0
+  let cut = 0;
+  while (wedgesNeeded > i) {
+    if (limes[0] == "small") {
+      i += 6
+    }
+    else if (limes[0] == "medium") {
+      i += 8
+    }
+    else if (limes[0] == "large") {
+      i += 10
+    }
+    else {
+      break;
+    }
+
+    limes.shift();
+    cut++;
+
+  }
+  return cut;
 }
 
 /**
@@ -34,5 +70,17 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Remove this line and implement the function');
+  const REP = [];
+  do {
+    //Break la boucle si le temps calculer est négatif
+
+    timeLeft -= timeToMixJuice(orders[0]);
+    REP.push(orders[0])
+    orders.shift();
+    if (orders.length == 0) {
+      break;
+    }
+  } while (timeLeft > 0)
+
+  return REP
 }
