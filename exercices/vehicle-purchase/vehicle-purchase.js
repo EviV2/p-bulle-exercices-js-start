63897d44989479202775f0c949c7085c12d5eef8
Elias <144105283+EviV2@users.noreply.github.com>
2025-12-19 13:20:49 +0100

    Feat: Corriger l'exercice (vehicle-purchase)
    [DONE][10]

--- exercices/vehicle-purchase/vehicle-purchase.js
+++ exercices/vehicle-purchase/vehicle-purchase.js
@@ -23,12 +23,18 @@ export function needsLicense(kind) {
  * @returns {string} a sentence of advice which option to choose
  */
 export function chooseVehicle(option1, option2) {
+  // if (option1 < option2) {
+  //   return option1 + " is clearly the bestter choice."
+  // }
+  // else {
+  //   return option2 + " is clearly the better choice."
+  // }
+  let l = option2
   if (option1 < option2) {
-    return option1 + " is clearly the better choice."
+    l = option1
   }
-  else {
-    return option2 + " is clearly the better choice."
-  }
+  return l + " is clearly the better choice.";
+
 }
 
 /**
